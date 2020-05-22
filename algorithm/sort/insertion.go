f3b6dfcd5ade25f8b745170139ee046a0e99a845
zhangkai <kai.zhang@star-link.com>
2020-05-22 10:09:08 +0800

    .

--- algorithm/sort/insertion.go
+++ algorithm/sort/insertion.go
@@ -8,7 +8,7 @@ func (s *Insertion) Name() string {
 
 func (s *Insertion) Sort(a []int) {
 	l := len(a)
-	for i := 1; i < l; i++ {
+	for i := 0; i < l; i++ {
 		for j := i; j > 0 && s.less(a[j], a[j-1]); j-- {
 			s.swap(&a[j], &a[j-1])
 		}
