bc7bf2ca886f044cb055ea475ac5fb59a358d416
zhangkai <kai.zhang@star-link.com>
2020-05-22 10:01:35 +0800

    .

--- algorithm/sort/selection.go
+++ algorithm/sort/selection.go
@@ -9,12 +9,12 @@ func (s *Selection) Name() string {
 func (s *Selection) Sort(a []int) {
 	l := len(a)
 	for i := 0; i < l; i++ {
-		min := i
-		for j := i; j < l; j++ {
-			if s.less(a[j], a[min]) {
-				min = j
+		m := i
+		for j := i + 1; j < l; j++ {
+			if s.less(a[j], a[m]) {
+				m = j
 			}
 		}
-		s.swap(&a[i], &a[min])
+		s.swap(&a[i], &a[m])
 	}
 }
